3612ceeb6375d999f97cab0cb561d92a3c20c6df
Daniel Dutton <dannyduttster@gmail.com>
2019-09-14 20:45:19 +0100

    Add custom TableViewCell
    Set identifier
    Add label and text field
    Set backing class and wire outlets to the same

---
+++ Stew's Stats/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  Stew's Stats
+//
+//  Created by Daniel Dutton on 14/09/2019.
+//  Copyright © 2019 Daniel Dutton. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblStat: UILabel!
+    
+    @IBOutlet weak var txtFieldStatValue: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
