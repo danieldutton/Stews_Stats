4f023331ff6ff0127ce4cc2547ab189a4b92ce5b
Daniel Dutton <dannyduttster@gmail.com>
2019-09-29 18:34:09 +0100

    Add 2 x BarButton Items to right navbar.  Pie & Calendar for annual stats
    Wire pie seque to tabViewController
    Wire calendar segue to new TableViewController

--- Stew's Stats/ViewController/StatisticSummaryTableVC.swift
+++ Stew's Stats/ViewController/StatisticSummaryTableVC.swift
@@ -18,7 +18,15 @@ class StatisticSummaryTableVC: UITableViewController {
 
     private var persistance = Persistance.shared
 
-
+    
+    @IBAction func onLaunchChartsClicked(_ sender: UIBarButtonItem) {
+        print(#function)
+    }
+    
+    @IBAction func onLaunchAnnualStatsClicked(_ sender: UIBarButtonItem) {
+        print(#function)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         seedData = StewsRuntasticData()
