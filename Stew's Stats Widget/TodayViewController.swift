cfba330135ff15a8cdb36216a9016f242415d946
Daniel Dutton <dannyduttster@gmail.com>
2019-10-04 21:46:23 +0100

    Basic Today extension added and working with three empty labels

---
+++ Stew's Stats Widget/TodayViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TodayViewController.swift
+//  Stew's Stats Widget
+//
+//  Created by Daniel Dutton on 04/10/2019.
+//  Copyright © 2019 Daniel Dutton. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    @IBOutlet weak var lbl_1: UILabel!
+    
+    @IBOutlet weak var lbl_2: UILabel!
+    
+    @IBOutlet weak var lbl_3: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setLabelPlaceholderText()
+    }
+    
+    private func setLabelPlaceholderText() {
+        let placeHolderText = "--"
+        
+        lbl_1.text = placeHolderText
+        lbl_2.text = placeHolderText
+        lbl_3.text = placeHolderText
+    }
+
+    private func displayData() {
+        
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
